f359171326beb83b870669f34b66d9eb0260c994
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2019-04-28 20:06:27 +0200

    Mockito - use @Spy

--- src/test/java/com/pskwiercz/unittest/mockitospringboot/busines/ListMockTest.java
+++ src/test/java/com/pskwiercz/unittest/mockitospringboot/busines/ListMockTest.java
@@ -4,8 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -17,9 +19,12 @@ public class ListMockTest {
 
     @Mock
     List mock;
-
     @Mock
     List<String> mockListStrings;
+    @Mock
+    ArrayList mockArrayList;
+    @Spy
+    ArrayList spyMockArrayList;
 
     @Test
     public void mockReturnOneValueTest() {
@@ -86,5 +91,36 @@ public class ListMockTest {
 
         assertEquals("Example string 1", captureValues.get(0));
         assertEquals("Example string 2", captureValues.get(1));
+    }
+
+    @Test
+    public void mockingTest() {
+        mockArrayList.get(0); // null
+        mockArrayList.size(); // 0
+        mockArrayList.add("Test1");
+        mockArrayList.add("Test2");
+        mockArrayList.size(); //!!!!!!! stil 0 THIS IS MOCK NOT REAL OBJECT !!!!!!
+        when(mockArrayList.size()).thenReturn(5);
+        mockArrayList.size(); // 5 !!!!
+    }
+
+    @Test
+    public void spyingTest() {
+        spyMockArrayList.add("Test0");
+        spyMockArrayList.get(0); // Test0
+        spyMockArrayList.size(); // 1
+        spyMockArrayList.add("Test1");
+        spyMockArrayList.add("Test2");
+        spyMockArrayList.size(); // 3
+
+        when(spyMockArrayList.size()).thenReturn(5);
+        System.out.println(spyMockArrayList.size()); // 5 !!!!
+
+        // NOTICE
+        spyMockArrayList.add("Test3");
+        System.out.println(spyMockArrayList.size()); // STILL 5 !!!!!
+
+        verify(spyMockArrayList).add("Test2");
+        verify(spyMockArrayList, never()).add("Test4");
     }
 }

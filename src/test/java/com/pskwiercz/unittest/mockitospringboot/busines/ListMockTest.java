fd74bcb4e7f51de8f7413556a6717da1552db67e
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2019-04-28 19:28:42 +0200

    Mockito - use verify()

--- src/test/java/com/pskwiercz/unittest/mockitospringboot/busines/ListMockTest.java
+++ src/test/java/com/pskwiercz/unittest/mockitospringboot/busines/ListMockTest.java
@@ -9,13 +9,16 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ListMockTest {
 
     @Mock
     List mock;
+
+    @Mock
+    List<String> mockListStrings;
 
     @Test
     public void mockReturnOneValueTest() {
@@ -42,5 +45,19 @@ public class ListMockTest {
         when(mock.get(anyInt())).thenReturn("Example String");
         assertEquals("Example String", mock.get(0));
         assertEquals("Example String", mock.get(1));
+    }
+
+    @Test
+    public void verificationBasics() {
+        String value1 = mockListStrings.get(0);
+        String value2 = mockListStrings.get(1);
+
+        // Verify
+        verify(mockListStrings).get(0);
+        verify(mockListStrings, times(2)).get(anyInt());
+        verify(mockListStrings, atLeast(1)).get(anyInt());
+        verify(mockListStrings, atLeastOnce()).get(anyInt());
+        verify(mockListStrings, atMost(2)).get(anyInt());
+        verify(mockListStrings, never()).get(2);
     }
 }

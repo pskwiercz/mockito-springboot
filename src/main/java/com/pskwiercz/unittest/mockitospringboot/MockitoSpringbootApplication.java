482ef8c7c08c447d8fa3cd17086564e5c8790911
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2019-04-28 14:01:43 +0200

    Initial commit

---
+++ src/main/java/com/pskwiercz/unittest/mockitospringboot/MockitoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pskwiercz.unittest.mockitospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoSpringbootApplication.class, args);
+    }
+
+}
